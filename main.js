972c680518fd457a44c4c5f26954a2a1931c6b9d
Sofia <Perlari>
2020-11-09 13:16:03 +0100

    svolgimento slider con vue, nella versione base con loop

---
+++ main.js
@@ -0,0 +1,34 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            indice_immagine: 0,
+            immagini: [
+                "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg",
+                "https://profoundponders.com/wp-content/uploads/2020/07/lotus-flower-828457262-5c6334b646e0fb0001dcd75a.jpg",
+                "https://images.pexels.com/photos/658687/pexels-photo-658687.jpeg?cs=srgb&dl=pexels-cindy-gustafson-658687.jpg&fm=jpg",
+                "https://cdn.britannica.com/84/73184-004-E5A450B5/Sunflower-field-Fargo-North-Dakota.jpg"
+            ]
+        },
+        methods: {
+            next_image() {
+                // incremento l'indice dell'immagine corrente
+                this.indice_immagine += 1;
+                // verifico l'indice è uscito dal range delle posizioni dell'array
+                // ossia se ho superato il limite superiore
+                if(this.indice_immagine > this.immagini.length - 1) {
+                    this.indice_immagine = 0;
+                }
+            },
+            prev_image() {
+                // decremento l'indice dell'immagine corrente
+                this.indice_immagine -= 1;
+                // verifico se l'indice è uscito dal range delle posizioni dell'array
+                // ossia se ho superato il limite inferiore
+                if(this.indice_immagine < 0) {
+                    this.indice_immagine = this.immagini.length - 1;
+                }
+            }
+        }
+    }
+);
